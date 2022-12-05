b7b9389e1efc1f15ac7b1faec422beb3090eedb0
Baptiste Gauthier-Martinet <bgauthie@z2r8p6.42lyon.fr>
2022-12-05 16:29:30 +0100

    GNL

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgauthie <bgauthie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 15:20:19 by bgauthie          #+#    #+#             */
+/*   Updated: 2022/12/05 15:23:53 by bgauthie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen_nocrash(const char *s);
+size_t	ft_strchr_nl(const char *s, int c);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
+
+char	*get_next_line(int fd);
+char	*join_and_free(char	*s1, char	*s2);
+char	*join_buffer(int fd, char *str);
+char	*cut_temp(char	*str);
+char	*stock_supp(char *str);
+
+#endif
